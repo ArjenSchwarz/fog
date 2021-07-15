863a354eb2c2d9b39063f8b1670cc1f80ee9baf3
Arjen Schwarz <arjen.schwarz@digio.com.au>
2021-07-15 22:21:40 +1000

    Add command to list all CloudFormation managed resources

---
+++ cmd/resources.go
@@ -0,0 +1,85 @@
+/*
+Copyright © 2021 Arjen Schwarz <developer@arjen.eu>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/ArjenSchwarz/fog/config"
+	"github.com/ArjenSchwarz/fog/lib"
+	"github.com/ArjenSchwarz/fog/lib/format"
+	"github.com/spf13/cobra"
+)
+
+// resourcesCmd represents the resources command
+var resourcesCmd = &cobra.Command{
+	Use:   "resources",
+	Short: "List all CloudFormation managed resources",
+	Long: `This command let's you see all the resources managed by your CloudFormation templates.
+
+The standard output shows the type, resource ID, and stack it's managed by. Verbose mode adds the logical ID in the CloudFormation stack and the status.
+Using the stackname argument you can limit this to a specific stack using the stack's name or ID. If you provide a wildcard filter such as "*dev*" it will match all stacks that match that pattern.
+
+Examples:
+
+$ fog resources
+$ fog resources --stackname my-awesome-stack
+$ fog resources --stackname "*awesome*"`,
+	Run: listResources,
+}
+
+var resource_stackname *string
+
+func init() {
+	rootCmd.AddCommand(resourcesCmd)
+	resource_stackname = resourcesCmd.Flags().StringP("stackname", "n", "", "Name, ID, or wildcard filter for the stack (optional)")
+}
+
+func listResources(cmd *cobra.Command, args []string) {
+	awsConfig := config.DefaultAwsConfig(*settings)
+	resources := lib.GetResources(resource_stackname, awsConfig.CloudformationClient())
+	keys := []string{"Type", "ID", "Stack"}
+	if settings.GetBool("verbose") {
+		keys = append(keys, []string{"LogicalID", "Status"}...)
+	}
+	subtitle := "All resources created by CloudFormation"
+	if *resource_stackname != "" {
+		subtitle = fmt.Sprintf("Resources for %v", *resource_stackname)
+	}
+	title := fmt.Sprintf("%v in account %v for region %v", subtitle, awsConfig.AccountID, awsConfig.Region)
+	output := format.OutputArray{Keys: keys, Title: title}
+	output.SortKey = "Type"
+	for _, resource := range resources {
+		content := make(map[string]string)
+		content["Type"] = resource.Type
+		content["ID"] = resource.ResourceID
+		content["Stack"] = resource.StackName
+		if settings.GetBool("verbose") {
+			content["LogicalID"] = resource.LogicalID
+			content["Status"] = resource.Status
+		}
+		holder := format.OutputHolder{Contents: content}
+		output.AddHolder(holder)
+	}
+	output.Write(*settings)
+
+}
---
+++ lib/resources.go
@@ -0,0 +1,88 @@
+package lib
+
+import (
+	"context"
+	"errors"
+	"log"
+	"regexp"
+	"strings"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/service/cloudformation"
+	"github.com/aws/aws-sdk-go-v2/service/cloudformation/types"
+	"github.com/aws/smithy-go"
+)
+
+type CfnResource struct {
+	StackName  string
+	Type       string
+	ResourceID string
+	LogicalID  string
+	Status     string
+}
+
+// GetResources returns all the exports in the account and region. If stackname
+// is provided, results will be limited to that stack.
+func GetResources(stackname *string, svc *cloudformation.Client) []CfnResource {
+	input := &cloudformation.DescribeStacksInput{}
+	if *stackname != "" && !strings.Contains(*stackname, "*") {
+		input.StackName = stackname
+	}
+	resp, err := svc.DescribeStacks(context.TODO(), input)
+	if err != nil {
+		var bne *smithy.OperationError
+		if errors.As(err, &bne) {
+			log.Fatalln("error:", bne.Err)
+		}
+		log.Fatalln(err)
+	}
+	stackRegex := "^" + strings.Replace(*stackname, "*", ".*", -1) + "$"
+	tocheckstacks := make([]types.Stack, 0)
+	for _, stack := range resp.Stacks {
+		if strings.Contains(*stackname, "*") {
+			if matched, _ := regexp.MatchString(stackRegex, *stack.StackName); !matched {
+				continue
+			}
+		}
+		tocheckstacks = append(tocheckstacks, stack)
+	}
+	resourcelist := make([]CfnResource, 0)
+	for _, stack := range tocheckstacks {
+		resources, err := svc.DescribeStackResources(
+			context.TODO(),
+			&cloudformation.DescribeStackResourcesInput{StackName: stack.StackName})
+		if err != nil {
+			var ae smithy.APIError
+			if errors.As(err, &ae) {
+				// If the error is because of throttling, we'll wait 5 seconds before trying the same query again
+				if ae.ErrorCode() == "Throttling" && ae.ErrorMessage() == "Rate exceeded" {
+					time.Sleep(5 * time.Second)
+					resources, err = svc.DescribeStackResources(
+						context.TODO(),
+						&cloudformation.DescribeStackResourcesInput{StackName: stack.StackName})
+					// If it still fails though, we'll just break down
+					if err != nil {
+						log.Fatalln(err)
+					}
+				} else {
+					// If it's another type of API error, we fail on it
+					log.Fatalf("code: %s, message: %s, fault: %s", ae.ErrorCode(), ae.ErrorMessage(), ae.ErrorFault().String())
+				}
+			} else {
+				// If it's a completely different type of error, we also fail
+				log.Fatalln(err)
+			}
+		}
+		for _, resource := range resources.StackResources {
+			resitem := CfnResource{
+				StackName:  *stack.StackName,
+				Type:       *resource.ResourceType,
+				ResourceID: *resource.PhysicalResourceId,
+				LogicalID:  *resource.LogicalResourceId,
+				Status:     string(resource.ResourceStatus),
+			}
+			resourcelist = append(resourcelist, resitem)
+		}
+	}
+	return resourcelist
+}
