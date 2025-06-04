890b47225f9048b4d656943d648e35d3ab3fea07
Arjen Schwarz <developer@arjen.eu>
2025-06-05 00:42:09 +1000

    docs: clarify identitycenter tests

--- lib/interfaces.go
+++ lib/interfaces.go
@@ -4,6 +4,8 @@ import (
 	"context"
 
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	"github.com/aws/aws-sdk-go-v2/service/organizations"
+	"github.com/aws/aws-sdk-go-v2/service/ssoadmin"
 )
 
 type EC2DescribeNaclsAPI interface {
@@ -16,4 +18,20 @@ type EC2DescribeRouteTablesAPI interface {
 
 type EC2DescribeManagedPrefixListsAPI interface {
 	DescribeManagedPrefixLists(ctx context.Context, params *ec2.DescribeManagedPrefixListsInput, optFns ...func(*ec2.Options)) (*ec2.DescribeManagedPrefixListsOutput, error)
+}
+
+type SSOAdminListInstancesAPI interface {
+	ListInstances(ctx context.Context, params *ssoadmin.ListInstancesInput, optFns ...func(*ssoadmin.Options)) (*ssoadmin.ListInstancesOutput, error)
+}
+
+type SSOAdminListPermissionSetsAPI interface {
+	ListPermissionSets(ctx context.Context, params *ssoadmin.ListPermissionSetsInput, optFns ...func(*ssoadmin.Options)) (*ssoadmin.ListPermissionSetsOutput, error)
+}
+
+type SSOAdminListAccountAssignmentsAPI interface {
+	ListAccountAssignments(ctx context.Context, params *ssoadmin.ListAccountAssignmentsInput, optFns ...func(*ssoadmin.Options)) (*ssoadmin.ListAccountAssignmentsOutput, error)
+}
+
+type OrganizationsListAccountsAPI interface {
+	ListAccounts(ctx context.Context, params *organizations.ListAccountsInput, optFns ...func(*organizations.Options)) (*organizations.ListAccountsOutput, error)
 }
