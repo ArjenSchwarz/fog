f655c497ff69f28e281f8296ec2f350b70915f68
Arjen Schwarz <developer@arjen.eu>
2025-06-08 14:51:59 +1000

    [feature] use config for extension validation

---
+++ cmd/flags/groups/stack.go
@@ -0,0 +1,10 @@
+package groups
+
+import "github.com/ArjenSchwarz/fog/cmd/flags"
+
+// StackFlags will hold flags for stack-related commands.
+type StackFlags struct {
+	*flags.BaseFlagValidator
+}
+
+// TODO: implement stack flag definitions and validation rules.
